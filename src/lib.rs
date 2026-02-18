2ab712587ef0da48adf10cf8d211ca9eb308a8c1
Julien Rolland <julien.rolland@universite-paris-saclay.fr>
2026-02-18 16:52:57 +0100

    Add crate-level docs with usage examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! Tiny ring-buffer types specialized for exactly two elements.
+//!
+//! This crate provides:
+//! - [`RingPair`], storing values inline as `[T; 2]`
+//! - [`BoxedRingPair`], storing values on the heap as `Box<[T; 2]>`
+//!
+//! # Examples
+//! ```rust
+//! use ring_pair::{BoxedRingPair, RingPair};
+//!
+//! let mut inline = RingPair::new(1);
+//! inline.push(2);
+//! assert_eq!(inline.as_pair(), (&1, &2));
+//!
+//! let mut boxed = BoxedRingPair::new(String::from("a"));
+//! boxed.push(String::from("b"));
+//! assert_eq!(boxed.as_pair(), (&String::from("a"), &String::from("b")));
+//! ```
+
 mod ring_pair;
 mod boxed_ring_pair;
 
